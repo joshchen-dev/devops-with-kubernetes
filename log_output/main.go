2e1c33f37f170f1d947838db8b5e7338f734aa7b
Josh Chen <josh@joshchen.dev>
2026-06-20 17:49:18 +0900

    exercise 1.7 done

--- log_output/main.go
+++ log_output/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 	"time"
 
 	"github.com/google/uuid"
@@ -13,7 +15,19 @@ func main() {
 	ticker := time.NewTicker(5000 * time.Millisecond)
 	defer ticker.Stop()
 
-	for range ticker.C {
+	go reportRandomString(rs, ticker)
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/now", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, time.Now().Format(time.RFC3339Nano)+" "+rs)
+	})
+
+	log.Fatal(http.ListenAndServe(":3000", mux))
+}
+
+func reportRandomString(rs string, t *time.Ticker) {
+	for range t.C {
 		log.Println(rs)
 	}
 }
