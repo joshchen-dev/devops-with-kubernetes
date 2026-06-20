a6fab894c383d12b61b5e5e33900030ad3351c65
Josh Chen <josh@joshchen.dev>
2026-06-20 13:39:15 +0900

    exercise 1.5 done

--- todo_app/main.go
+++ todo_app/main.go
@@ -10,6 +10,14 @@ import (
 func main() {
 	PORT := os.Getenv("PORT")
 	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			http.ServeFile(w, r, "static/index.html")
+		}
+	})
+
 	fmt.Printf("Server started in port %s\n", PORT)
 
 	log.Fatal(http.ListenAndServe(":"+PORT, mux))
